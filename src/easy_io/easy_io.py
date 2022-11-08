b67d23ca829f3e63a3076c78e4a3751bb2a3b3c9
ryokamoi <ryokamoi.jp@gmail.com>
2022-11-08 12:44:38 -0600

    integrated list read/dump to read_list_from_test_file and dump_list_to_text_file

--- src/easy_io/easy_io.py
+++ src/easy_io/easy_io.py
@@ -1,4 +1,4 @@
-from typing import Union, List
+from typing import Union, List, Literal
 import json
 
 from pathlib import Path
@@ -13,14 +13,18 @@ def read_jsonl(dataset_path: Union[Path, str]) -> List[dir]:
     return dataset_json_list
 
 
-def read_numbers_list_from_text_file(file_path: Union[Path, str], num_type="int") -> List[int]:
+def read_list_from_text_file(file_path: Union[Path, str], element_type: Literal["int", "float", "str"]) -> List[int]:
     output = []
     with open(file_path, "r") as f:
         for line_ in f.readlines():
+            assert line_[-1] == "\n"
+            
             line = line_[:-1]
-            if num_type == "int":
+            if element_type == "str":
+                output.append(line)
+            elif element_type == "int":
                 output.append(int(line))
-            elif num_type == "float":
+            elif element_type == "float":
                 output.append(float(line))
             else:
                 raise ValueError("Not implemented")
@@ -28,19 +32,7 @@ def read_numbers_list_from_text_file(file_path: Union[Path, str], num_type="int"
     return output
 
 
-def read_str_list_from_text_file(file_path: Union[Path, str]) -> List[str]:
-    output: List[str] = []
-    
-    with open(file_path, "r") as f:
-        for line_ in f.readlines():
-            assert line_[-1] == "\n"
-            
-            output.append(line_[:-1])
-    
-    return output
-
-
-def dump_numbers_list_to_text_file(num_list: List[Union[float, int]], file_path: Union[Path, str]):
+def dump_list_to_text_file(dump_list: List[Union[float, int, str]], file_path: Union[Path, str]):
     with open(file_path, "w") as f:
-        for num in num_list:
+        for num in dump_list:
             f.write(str(num) + "\n")
